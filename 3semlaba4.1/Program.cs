021831f06834ca4301559a6bd6cffd1786a910f3
Viktoria Tambovskaya <viktoria.tambovskaya@yandex.ru>
2020-12-23 20:00:31 +0700

    add second number

--- 3semlaba4.1/Program.cs
+++ 3semlaba4.1/Program.cs
@@ -10,6 +10,8 @@ namespace _3semlaba4._1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Введите второе число: ");
+            double B = Convert.ToDouble(Console.ReadLine());
         }
     }
 }
